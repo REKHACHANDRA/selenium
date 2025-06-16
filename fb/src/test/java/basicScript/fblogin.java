624ac3e15846dbb0da356b5525b3af6ed2cd5df0
DELL <DELL@DESKTOP-A0FLEGI.lan>
2025-06-16 21:56:56 +0530

    facebook test case

---
+++ fb/src/test/java/basicScript/fblogin.java
@@ -0,0 +1,15 @@
+package basicScript;
+
+import org.testng.annotations.Test;
+
+
+public class fblogin 
+{
+	@Test
+	public void fb1()
+	{
+		System.out.println("good evening Rekha.c its working!! ");
+	}
+ 
+
+}
